6aa25ff35d11168906851158ffb161898bf4aaf7
Barunangshu Bhowmik <barunangshu.bhowmik.ece26@heritageit.edu.in>
2025-06-21 22:11:14 +0530

    modify hero and header section

--- components/header.jsx
+++ components/header.jsx
@@ -14,10 +14,10 @@ const Header = async () => {
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/logo.png"}
+            src={"/aifinance2.png"}
             alt="Welth Logo"
-            width={200}
-            height={60}
+            width={400}
+            height={80}
             className="h-12 w-auto object-contain"
           />
         </Link>
--- components/hero.jsx
+++ components/hero.jsx
@@ -29,7 +29,7 @@ const HeroSection = () => {
   return (
     <section className="pt-40 pb-20 px-4">
       <div className="container mx-auto text-center">
-        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+        <h1 className="text-4xl md:text-6xl lg:text-[85px] pb-6 gradient-title">
           Manage Your Finances <br /> with Intelligence
         </h1>
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="#">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
@@ -51,9 +51,9 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.jpeg"
-              width={1280}
-              height={720}
+              src="/aifinance.png"
+              width={900}
+              height={320}
               alt="Dashboard Preview"
               className="rounded-lg shadow-2xl border mx-auto"
               priority
